314b323e41a71b1e5575d85e8a8d4d81a6a07634
ragokan <okanyldrmop@gmail.com>
2020-11-16 20:26:05 +0300

    url

--- frontend/src/context/utils/api.js
+++ frontend/src/context/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 // You can use your own backend, just change the baseURL to your url.
 
 const api = axios.create({
-  baseURL: "http://ragokan-mern.herokuapp.com/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
